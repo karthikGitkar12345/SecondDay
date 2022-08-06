3b7146e6a3ace5aba3de56a62c209d6ead55d460
karthikGitkar12345 <karthik.s110893@gmail.com>
2022-08-06 14:34:38 +0530

    Master

---
+++ src/com/git/FirstClass.java
@@ -0,0 +1,24 @@
+package com.git;
+
+public class FirstClass {
+
+	public void Name() {
+		System.out.println("Karthik Selladurai");
+	}
+
+	public void Course() {
+		System.out.println("Java Selenium");
+	}
+
+	public void Location() {
+		System.out.println("Thuraipakkam");
+	}
+	
+	public static void main(String[] args) {
+		FirstClass f = new FirstClass();
+		f.Name();
+		f.Course();
+		f.Location();
+	}
+
+}
